ebfd096ba529117e5b2d405a18adb2c49b4d9435
Gabriel Seals <sealsgj@gmail.com>
2020-01-07 19:58:53 -0600

    committing to stay up

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import boardData from '../../../helpers/data/boardData';
+import authData from '../../../helpers/data/authData';
+import Board from '../../shared/Board/Board';
 import './Home.scss';
 
 class Home extends React.Component {
+  state = {
+    boards: [],
+  }
+
+  componentDidMount() {
+    boardData.getBoardsByUid(authData.getUid())
+      .then((boards) => this.setState({ boards }))
+      .catch((err) => console.error('error from get boards', err));
+  }
+
   render() {
-    const boardId = '12345';
     return (
       <div className="Home">
         <h1>Home</h1>
-        <Link className="btn btn-primary" to="/board/new">Create New Board</Link>
-        <Link className="btn btn-secondary" to={`/board/${boardId}`}>Single Board Page</Link>
+        <div className="boards d-flex flex-wrap">
+          {this.state.boards.map((board) => <Board key={board.id} board={board} />)}
+        </div>
       </div>
     );
   }
